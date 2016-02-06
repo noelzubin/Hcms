9aaabfd42857d113c2966843fdb6642e2511c443
noelzubin <noelzubinvictor@gmail.com>
2016-02-06 13:52:00 +0530

    Set session driver to database

--- app/myfiles/general.php
+++ app/myfiles/general.php
@@ -2,6 +2,7 @@
 
 namespace App\myfiles;
 
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -24,6 +25,12 @@ class General{
     public static function getdocs($spec){
         $doctors = DB::select('select `id`,`queue`,`name` from ldoctors where speciality = "' . $spec .'"' );
         $doctors = self::getqueue($doctors);
+        $loggedInDoctors = self::getloggedinDocs();
+        foreach($doctors as $doctor){
+            $doctor->loggedin = 0;
+            if( in_array($doctor->id, $loggedInDoctors))
+                $doctor->loggedin = 1;
+        }
         return json_encode($doctors);
     }
 
@@ -39,6 +46,18 @@ class General{
         $docQ = $docQ .",".$pat;
         $docQ = DB::update('update ldoctors set queue = ? where id = ?', [$docQ,$doc]);
         return;
+    }
+
+    public static function getloggedinDocs(){
+        $docs = array();
+        $sesns = DB::select('select `payload` from sessions');
+        foreach($sesns as $sesn){
+            $data = unserialize(base64_decode($sesn->payload));
+            if(isset($data["loggedin"]) &&  ($data["loggedin"] == 1) ){
+                array_push($docs, $data["loggedUserId"] );
+            }
+        }
+        return $docs;
     }
 }
 
--- public/js/reception/patVisit.js
+++ public/js/reception/patVisit.js
@@ -12,7 +12,10 @@ $("#specSelect").change(function(){
                 data.sort(function(a,b){return a["queue"].length - b["queue"].length }); //sort
                 for (i = 0; i < data.length; i++) {
                     data[i]["queue"] = data[i]["queue"].filter(Boolean); //remove empty strings
-                    s = '<input type="radio" name="doctor" value="' + data[i]["id"] + '" >' + data[i]["name"] + '  ' + data[i]["queue"].length +'<br>';
+                    if(data[i]["loggedin"] == 1)
+                        s = '<input type="radio" name="doctor" value="' + data[i]["id"] + '" >' + data[i]["name"] + '  ' + data[i]["queue"].length +'   '+'Available<br>';
+                    else
+                        s = '<input type="radio" name="doctor" value="' + data[i]["id"] + '" >' + data[i]["name"] + '  ' + data[i]["queue"].length +'<br>';
                     $("#Doctors").append(s);
                 }
                 $("#Doctors").append("<input type='submit'/>");
